9f53bcac21884e7db2111bad4c780b9a15423394
Priyansh <priyanshgupta193@gmail.com>
2025-07-06 00:49:28 +0530

    Deployed on Vercel
    Cloudflare build removed

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-const nextConfig = {
-  output: 'standalone', // optional but good for custom builds
-  experimental: {
-    appDir: true, // only if using app directory
-  },
-};
-
-module.exports = nextConfig;
